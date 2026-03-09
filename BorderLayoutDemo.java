5d8dcbe64ed1609582f22a28505ab5535808791e
Mr Ahmed <mr.ahmed.coder@gmail.com>
2026-03-09 20:02:31 +0300

    Added comment explaining BorderLayout

--- BorderLayoutDemo.java
+++ BorderLayoutDemo.java
@@ -16,6 +16,7 @@ public class BorderLayoutDemo {
         JFrame borderFrame = new JFrame("BorderLayout Demo");
         borderFrame.setSize(600, 400);
         borderFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //manager for the frame
         borderFrame.setLayout(new BorderLayout());
 
         JLabel titleLabel = new JLabel("Application Title", JLabel.CENTER);
